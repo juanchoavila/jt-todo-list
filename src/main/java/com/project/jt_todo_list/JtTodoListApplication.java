160767968b69c96ef5c910429be3c8dd01fa558f
juanchoavila <ag.juandiego@gmail.com>
2024-05-27 20:42:33 -0500

    Initial commit

---
+++ src/main/java/com/project/jt_todo_list/JtTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.project.jt_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jt_todo_list/JtTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jt_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
