56b0ecfe4d77db4435ffa9f8636a379ff489e8a8
juanchoavila <ag.juandiego@gmail.com>
2024-05-29 20:46:35 -0500

    Add: Issue template

---
+++ src/test/java/com/project/jt_todo_list/Controller/PingControllerTest.java
@@ -0,0 +1,22 @@
+package com.project.jt_todo_list.Controller;
+
+import com.project.jt_todo_list.controller.PingController;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+public class PingControllerTest {
+
+    @Autowired
+    PingController pingController;
+
+    @Test
+    public void whenPingRequest_thenPongResponse() {
+        String pingResponse = pingController.ping();
+
+        assertThat(pingResponse).isEqualTo("pong");
+    }
+}
