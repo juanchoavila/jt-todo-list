b8f952934f0dd6ff6eea4c3e2798a1aa99eb804a
juanchoavila <ag.juandiego@gmail.com>
2024-06-03 13:37:55 -0500

    add: jacoco, modify: ci command to do test

--- src/test/java/com/project/jt_todo_list/Controller/PingControllerTest.java
+++ src/test/java/com/project/jt_todo_list/Controller/PingControllerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.util.Assert;
 
 @SpringBootTest
+@ContextConfiguration(classes = PingController.class)
 public class PingControllerTest {
 
     @Autowired
--- src/test/java/com/project/jt_todo_list/JtTodoListApplicationTests.java
+++ src/test/java/com/project/jt_todo_list/JtTodoListApplicationTests.java
@@ -2,8 +2,10 @@ package com.project.jt_todo_list;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = JtTodoListApplicationTests.class)
 class JtTodoListApplicationTests {
 
 	@Test
